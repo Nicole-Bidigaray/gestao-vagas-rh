6b9a693d79268ee0a33ddd8505ad116ea08df46c
Nicole Bidigaray <nicole_natsume@yahoo.com.br>
2026-01-29 19:02:39 -0300

    refactor(company): improve CompanyEntity validations and mapping

--- src/main/java/br/com/rh/gestaovagas/modules/company/entities/CompanyEntity.java
+++ src/main/java/br/com/rh/gestaovagas/modules/company/entities/CompanyEntity.java
@@ -1,39 +1,44 @@
 package br.com.rh.gestaovagas.modules.company.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.validator.constraints.Length;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
 
-@Entity(name = "company")
-@Data
+@Getter
+@Setter
+@Entity
+@Table(name = "companies")
 public class CompanyEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
-    @NotBlank()
+    @NotBlank(message = "O campo [username] é obrigatório")
     @Pattern(regexp = "\\S+", message = "O campo [username] não deve conter espaço")
     private String username;
 
+    @NotBlank(message = "O campo [email] é obrigatório")
     @Email(message = "O campo [email] deve conter um e-mail válido")
     private String email;
 
-    @Length(min = 10, max = 100, message = "A senha deve conter entre (10) e (100) caracteres")
+    @NotBlank(message = "O campo [password] é obrigatório")
+    @Length(min = 10, max = 100, message = "A senha deve conter entre 10 e 100 caracteres")
     private String password;
 
     private String website;
+
+    @NotBlank(message = "O campo [name] é obrigatório")
     private String name;
+
     private String description;
 
     @CreationTimestamp
