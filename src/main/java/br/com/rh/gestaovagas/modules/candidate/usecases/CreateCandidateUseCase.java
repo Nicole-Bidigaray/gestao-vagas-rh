9f689cd6f61f66cc3f23574e361aea7fe40b807c
Nicole Bidigaray <nicole_natsume@yahoo.com.br>
2026-02-02 15:00:24 -0300

    feat(candidate): encrypt candidate password before saving

--- src/main/java/br/com/rh/gestaovagas/modules/candidate/usecases/CreateCandidateUseCase.java
+++ src/main/java/br/com/rh/gestaovagas/modules/candidate/usecases/CreateCandidateUseCase.java
@@ -3,22 +3,28 @@ package br.com.rh.gestaovagas.modules.candidate.usecases;
 import br.com.rh.gestaovagas.exceptions.UserFoundException;
 import br.com.rh.gestaovagas.modules.candidate.CandidateEntity;
 import br.com.rh.gestaovagas.modules.candidate.CandidateRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CreateCandidateUseCase {
 
     private final CandidateRepository candidateRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    public CreateCandidateUseCase(CandidateRepository candidateRepository) {
+    public CreateCandidateUseCase(CandidateRepository candidateRepository, PasswordEncoder passwordEncoder) {
         this.candidateRepository = candidateRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public CandidateEntity execute(CandidateEntity candidate) {
-
-        if (candidateRepository.findByUsernameOrEmail(candidate.getUsername(), candidate.getEmail()).isPresent()) {
+        if (candidateRepository.findByUsernameOrEmail(candidate.getUsername(), candidate.getEmail())
+                .isPresent()) {
             throw new UserFoundException();
         }
+
+        var password = passwordEncoder.encode(candidate.getPassword());
+        candidate.setPassword(password);
 
         return candidateRepository.save(candidate);
     }
