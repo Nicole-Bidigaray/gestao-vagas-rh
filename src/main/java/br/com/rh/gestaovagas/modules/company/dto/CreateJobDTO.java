5fc2713241db2e54ec06ad45c5eb455ecd379109
Nicole Bidigaray <nicole_natsume@yahoo.com.br>
2026-02-02 14:16:08 -0300

    refactor: remove companyId from CreateJobDTO and use token context

--- src/main/java/br/com/rh/gestaovagas/modules/company/dto/CreateJobDTO.java
+++ src/main/java/br/com/rh/gestaovagas/modules/company/dto/CreateJobDTO.java
@@ -1,17 +1,7 @@
 package br.com.rh.gestaovagas.modules.company.dto;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-
-import java.util.UUID;
-
 public record CreateJobDTO(
         String description,
         String benefits,
-
-        @NotBlank(message = "O campo [level] é obrigatório")
-        String level,
-
-        @NotNull(message = "O campo [companyId] é obrigatório")
-        UUID companyId
+        String level
 ) {}
