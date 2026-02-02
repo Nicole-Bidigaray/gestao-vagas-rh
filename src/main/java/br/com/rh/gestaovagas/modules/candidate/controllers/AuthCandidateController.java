d5e459d97b0b34e86ccba2d98636c7a0e9194bcf
Nicole Bidigaray <nicole_natsume@yahoo.com.br>
2026-02-02 15:05:29 -0300

    feat(auth): add candidate authentication controller

---
+++ src/main/java/br/com/rh/gestaovagas/modules/candidate/controllers/AuthCandidateController.java
@@ -0,0 +1,31 @@
+package br.com.rh.gestaovagas.modules.candidate.controllers;
+
+import br.com.rh.gestaovagas.modules.candidate.dto.AuthCandidateRequestDTO;
+import br.com.rh.gestaovagas.modules.candidate.usecases.AuthCandidateUseCase;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthCandidateController {
+
+    private final AuthCandidateUseCase authCandidateUseCase;
+
+    public AuthCandidateController(AuthCandidateUseCase authCandidateUseCase) {
+        this.authCandidateUseCase = authCandidateUseCase;
+    }
+
+    @PostMapping("/candidate")
+    public ResponseEntity<Object> auth(@RequestBody AuthCandidateRequestDTO authCandidateRequestDTO) {
+        try {
+            var token = this.authCandidateUseCase.execute(authCandidateRequestDTO);
+            return ResponseEntity.ok().body(token);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(e.getMessage());
+        }
+    }
+}
