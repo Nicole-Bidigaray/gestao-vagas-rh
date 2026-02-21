e1b4ec6f4113f400578a462f92ffb2a4e089155c
Nicole Bidigaray <nicole_natsume@yahoo.com.br>
2026-02-21 18:15:43 -0300

    feat: adiciona documentação Swagger na entidade CandidateEntity

--- src/main/java/br/com/rh/gestaovagas/modules/candidate/CandidateEntity.java
+++ src/main/java/br/com/rh/gestaovagas/modules/candidate/CandidateEntity.java
@@ -1,5 +1,6 @@
 package br.com.rh.gestaovagas.modules.candidate;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
@@ -23,21 +24,26 @@ public class CandidateEntity {
     private UUID id;
 
     @NotBlank(message = "O campo [name] é obrigatório")
+    @Schema(example = "Daniel de Souza", requiredMode = Schema.RequiredMode.REQUIRED, description = "Nome do candidato")
     private String name;
 
     @NotBlank(message = "O campo [username] é obrigatório")
     @Pattern(regexp = "\\S+", message = "O campo [username] não deve conter espaço")
     @Length(min = 3, max = 50, message = "Username deve ter entre 3 e 50 caracteres")
+    @Schema(example = "daniel", requiredMode = Schema.RequiredMode.REQUIRED, description = "Username do candidato")
     private String username;
 
     @NotBlank(message = "O campo [email] é obrigatório")
     @Email(message = "O campo [email] deve conter um e-mail válido")
+    @Schema(example = "daniel@gmail.com", requiredMode = Schema.RequiredMode.REQUIRED, description = "E-mail do candidato")
     private String email;
 
     @NotBlank(message = "O campo [password] é obrigatório")
     @Length(min = 10, max = 100, message = "A senha deve conter entre 10 e 100 caracteres")
+    @Schema(example = "admin@1234", minLength = 10, maxLength = 100, requiredMode = Schema.RequiredMode.REQUIRED, description = "Senha do candidato")
     private String password;
 
+    @Schema(example = "Desevolvedor Java", requiredMode = Schema.RequiredMode.REQUIRED, description = "Breve descrição do candidato")
     private String description;
 
     private String curriculum;
