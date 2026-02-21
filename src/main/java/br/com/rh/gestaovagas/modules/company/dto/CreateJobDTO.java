8bafa3eeb258e92ed0d719f672ef9ba7a52e11c0
Nicole Bidigaray <nicole_natsume@yahoo.com.br>
2026-02-21 18:20:34 -0300

    feat: documenta campos do CreateJobDTO com exemplos no OpenAPI

--- src/main/java/br/com/rh/gestaovagas/modules/company/dto/CreateJobDTO.java
+++ src/main/java/br/com/rh/gestaovagas/modules/company/dto/CreateJobDTO.java
@@ -1,7 +1,15 @@
 package br.com.rh.gestaovagas.modules.company.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 public record CreateJobDTO(
+
+        @Schema(example = "Vaga para pessoa desenvolvedora júnior", requiredMode = Schema.RequiredMode.REQUIRED)
         String description,
+
+        @Schema(example = "GYMPass, Plano de saúde", requiredMode = Schema.RequiredMode.REQUIRED)
         String benefits,
+
+        @Schema(example = "JUNIOR", requiredMode = Schema.RequiredMode.REQUIRED)
         String level
 ) {}
