0634bc07ec69be6b3e6b782aede99be36298c474
Nicole Bidigaray <nicole_natsume@yahoo.com.br>
2026-02-21 18:11:20 -0300

    feat: adiciona documentação Swagger e endpoint de listagem de vagas para candidato

--- src/main/java/br/com/rh/gestaovagas/modules/candidate/controllers/CandidateController.java
+++ src/main/java/br/com/rh/gestaovagas/modules/candidate/controllers/CandidateController.java
@@ -1,30 +1,49 @@
 package br.com.rh.gestaovagas.modules.candidate.controllers;
 
 import br.com.rh.gestaovagas.modules.candidate.CandidateEntity;
+import br.com.rh.gestaovagas.modules.candidate.dto.ProfileCandidateResponseDTO;
 import br.com.rh.gestaovagas.modules.candidate.usecases.CreateCandidateUseCase;
+import br.com.rh.gestaovagas.modules.candidate.usecases.ListAllJobsByFilterUseCase;
 import br.com.rh.gestaovagas.modules.candidate.usecases.ProfileCandidateUseCase;
+import br.com.rh.gestaovagas.modules.company.entities.JobEntity;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.UUID;
 
 @RestController
 @RequestMapping("/candidate")
+@Tag(name = "Candidato", description = "Informações do candidato")
 public class CandidateController {
 
     private final CreateCandidateUseCase createCandidateUseCase;
 
     private final ProfileCandidateUseCase profileCandidateUseCase;
 
-    public CandidateController(CreateCandidateUseCase createCandidateUseCase, ProfileCandidateUseCase profileCandidateUseCase) {
+    private final ListAllJobsByFilterUseCase listAllJobsByFilterUseCase;
+
+    public CandidateController(CreateCandidateUseCase createCandidateUseCase, ProfileCandidateUseCase profileCandidateUseCase, ListAllJobsByFilterUseCase listAllJobsByFilterUseCase) {
         this.createCandidateUseCase = createCandidateUseCase;
         this.profileCandidateUseCase = profileCandidateUseCase;
+        this.listAllJobsByFilterUseCase = listAllJobsByFilterUseCase;
     }
 
     @PostMapping("/")
+    @Operation(summary = "Cadastro de candidato", description = "Essa função é responsável por cadastrar um candidato")
+    @ApiResponse(responseCode = "200",
+            content = @Content(schema = @Schema(implementation = CandidateEntity.class)))
+    @ApiResponse(responseCode = "400", description = "Usuário já existe")
     public ResponseEntity<Object> create(@Valid @RequestBody CandidateEntity candidateEntity) {
         try {
             var result = this.createCandidateUseCase.execute(candidateEntity);
@@ -36,6 +55,12 @@ public class CandidateController {
 
     @GetMapping("/")
     @PreAuthorize("hasRole('CANDIDATE')")
+    @Operation(summary = "Perfil do candidato", description = "Essa função é responsável por buscar as informações do perfil do candidato")
+    @ApiResponse(responseCode = "200",
+            content = @Content(schema = @Schema(implementation = ProfileCandidateResponseDTO.class)))
+    @ApiResponse(responseCode = "400", description = "User not found")
+
+    @SecurityRequirement(name = "jwt_auth")
     public ResponseEntity<Object> get(HttpServletRequest request) {
         var idCandidate = request.getAttribute("candidate_id");
         try {
@@ -45,5 +70,16 @@ public class CandidateController {
         } catch (Exception e) {
             return ResponseEntity.badRequest().body(e.getMessage());
         }
+    }
+
+    @GetMapping("/job")
+    @PreAuthorize("hasRole('CANDIDATE')")
+    @Operation(summary = "Listagem de vagas disponível para o candidato", description = "Essa função é responsável por listar todas as vagas disponíveis, baseada no filtro")
+    @ApiResponse(responseCode = "200", content = {
+            @Content(array = @ArraySchema(schema = @Schema(implementation = JobEntity.class)))
+    })
+    @SecurityRequirement(name = "jwt_auth")
+    public List<JobEntity> findJobByFilter(@RequestParam String filter) {
+        return this.listAllJobsByFilterUseCase.execute(filter);
     }
 }
