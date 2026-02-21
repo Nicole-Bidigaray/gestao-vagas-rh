13920b5bd72c83974de5d2262c5a92630ff669de
Nicole Bidigaray <nicole_natsume@yahoo.com.br>
2026-02-21 18:12:50 -0300

    feat: adiciona busca de vagas por descrição com join fetch no JobRepository

--- src/main/java/br/com/rh/gestaovagas/modules/company/repositories/JobRepository.java
+++ src/main/java/br/com/rh/gestaovagas/modules/company/repositories/JobRepository.java
@@ -2,8 +2,19 @@ package br.com.rh.gestaovagas.modules.company.repositories;
 
 import br.com.rh.gestaovagas.modules.company.entities.JobEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface JobRepository extends JpaRepository<JobEntity, UUID> {
+
+    @Query("""
+    SELECT j
+    FROM JobEntity j
+    JOIN FETCH j.company
+    WHERE LOWER(j.description) LIKE LOWER(CONCAT('%', :filter, '%'))
+  """)
+    List<JobEntity> findByDescriptionWithCompany(@Param("filter") String filter);
 }
