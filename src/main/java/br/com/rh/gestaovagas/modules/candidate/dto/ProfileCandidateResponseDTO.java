180de2aba56f40a059adbe8a80ddce9a15652a21
Nicole Bidigaray <nicole_natsume@yahoo.com.br>
2026-02-21 18:21:30 -0300

    feat: adiciona documentação Swagger no ProfileCandidateResponseDTO

--- src/main/java/br/com/rh/gestaovagas/modules/candidate/dto/ProfileCandidateResponseDTO.java
+++ src/main/java/br/com/rh/gestaovagas/modules/candidate/dto/ProfileCandidateResponseDTO.java
@@ -1,12 +1,22 @@
 package br.com.rh.gestaovagas.modules.candidate.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 import java.util.UUID;
 
 public record ProfileCandidateResponseDTO(
+
+    @Schema(example = "Desenvolvedora Java")
     String description,
+
+    @Schema(example = "maria")
     String username,
+
+    @Schema(example = "maria@gmail.com")
     String email,
     UUID id,
+
+    @Schema(example = "Maria de Souza")
     String name
 ) {
 }
