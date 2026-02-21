c5884d68bd569bf6005c42ae0801b14c81bf266a
Nicole Bidigaray <nicole_natsume@yahoo.com.br>
2026-02-21 18:14:27 -0300

    feat: adiciona documentação Swagger no JobController

--- src/main/java/br/com/rh/gestaovagas/modules/company/controllers/JobController.java
+++ src/main/java/br/com/rh/gestaovagas/modules/company/controllers/JobController.java
@@ -4,6 +4,12 @@ import br.com.rh.gestaovagas.modules.company.dto.CreateJobDTO;
 import br.com.rh.gestaovagas.modules.company.dto.JobResponseDTO;
 import br.com.rh.gestaovagas.modules.company.entities.JobEntity;
 import br.com.rh.gestaovagas.modules.company.usecases.CreateJobUseCase;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
@@ -25,6 +31,12 @@ public class JobController {
 
     @PostMapping
     @PreAuthorize("hasRole('COMPANY')")
+    @Tag(name = "Vagas", description = "Informações das vagas")
+    @Operation(summary = "Cadastro das vagas", description = "Essa função é responsável por cadastrar as vagas dentro da empresa")
+    @ApiResponse(responseCode = "200", content = {
+            @Content(schema = @Schema(implementation = JobEntity.class))
+    })
+    @SecurityRequirement(name = "jwt_auth")
     public ResponseEntity<JobResponseDTO> create(@Valid @RequestBody CreateJobDTO dto,
                                                  HttpServletRequest request) {
 
